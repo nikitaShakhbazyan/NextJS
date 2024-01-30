aeb1b07842893123a3780812c1f2eb37546741a1
nickshakh <shakhbazyann@gmail.com>
2024-01-30 15:25:04 +0400

    added google auth and privat routes

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,12 +1,16 @@
 import type { AuthOptions } from "next-auth";
 import Github from "next-auth/providers/github";
-
+import Google from "next-auth/providers/google";
 
 export const authConfig : AuthOptions = {
     providers : [
         Github({
             clientId:process.env.AUTH_GITHUB_ID!,
             clientSecret:process.env.AUTH_GITHUB_SECRET!
-        })
+        }),
+         Google ({
+            clientId:process.env.AUTH_GOOGLE_ID!,
+            clientSecret:process.env.AUTH_GOOGLE_SECRET!
+         })
     ]
 }
